75886df3fdca70aac4b499c01570293443e8eaa2
LIM CHIN BOON <chinboon.lim04@gmail.com>
2025-11-22 13:41:07 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+// js/main.js
 
+import { fetchSheetData } from './data.js'; // fetch function from data.js
+
+// Test fetching data
+fetchSheetData().then(data => console.log(data));
+
+// Later we will add code here to create 3D tiles using three.js
